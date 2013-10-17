de7b7e2333c59e5bd459b2a19297e1ec8dc8f190
unknown <chschulz@Chschulz-0731.redmond.corp.microsoft.com>
2013-10-17 11:17:17 -0700

    The site werks

---
+++ website/Gruntfile.js
@@ -0,0 +1,61 @@
+(function() {
+    "use strict";
+
+    module.exports = function( grunt ){
+
+        // Project Configuration
+        grunt.initConfig({
+             pkg: grunt.file.readJSON( "package.json" ),
+
+             // Uglify JS
+            uglify: {
+                options: { },
+                dist: {
+                    files: {
+                         // "public/dest/file.js" : ["source.js"]
+                    }
+                }
+            },
+
+            // Compile LESS
+            less: {
+                development: {
+                    options: {
+                        compress: true
+                    },
+                    files: {
+                         "css/site.min.css": "theme/site.less"
+                    }
+                }
+            },
+
+            // Linting Files / Directories
+            jshint: {
+                all: ["Gruntfile.js", "lib/*.js"],
+                options: {
+                    eqeqeq: true,
+                    quotmark: "double",
+                    unused: true,
+                    strict: true,
+                    trailing: true
+                }
+            },
+
+            // Watch Directories / Files
+            watch: {
+                files: ["Gruntfile.js", "lib/*.js", "theme/*.less"],
+                tasks: ["default"]
+            }
+        });
+
+        // Load the plugins
+        grunt.loadNpmTasks( "grunt-contrib-uglify" );
+        grunt.loadNpmTasks( "grunt-contrib-less" );
+        grunt.loadNpmTasks( "grunt-contrib-jshint" );
+        grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+        // Default tasks
+        grunt.registerTask( "default", ["uglify", "less", "jshint"] );
+    };
+
+})();
