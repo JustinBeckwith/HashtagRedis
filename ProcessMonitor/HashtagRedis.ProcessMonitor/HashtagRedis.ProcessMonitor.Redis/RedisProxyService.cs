1372e340fb82508b13fdd4c40d5821cda67a8bd5
= <nowakra@gmail.com>
2013-10-17 12:13:12 -0700

    deleting unused files

--- ProcessMonitor/HashtagRedis.ProcessMonitor/HashtagRedis.ProcessMonitor.Redis/RedisProxyService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.Composition;
-
-namespace HashtagRedis.ProcessMonitor.Redis
-{
-    [Export(typeof(IService))]
-    internal class RedisProxyService : Service
-    {
-    }
-}
