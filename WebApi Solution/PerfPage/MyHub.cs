2a5d4684cecfdee0e5b19c40faf48d32228d091b
David Anson <davidans@microsoft.com>
2013-10-17 15:59:51 -0700

    Add perf page.

---
+++ WebApi Solution/PerfPage/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PerfPage
+{
+    public class MyHub : Hub
+    {
+        public void RedisTests()
+        {
+            Clients.All.showmessage("woot");
+        }
+    }
+}
