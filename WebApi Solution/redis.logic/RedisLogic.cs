f3efbb0a56543b9f029db68910a7ce4a697f363f
David Anson <davidans@microsoft.com>
2013-10-17 12:52:08 -0700

    Update logic to call process manager.

--- WebApi Solution/redis.logic/RedisLogic.cs
+++ WebApi Solution/redis.logic/RedisLogic.cs
@@ -1,31 +1,45 @@
-﻿using System.Collections.Concurrent;
+﻿using redis.process;
+using System.Collections.Concurrent;
 using System.Threading;
 
 namespace redis.logic
 {
     static class Manager
     {
+        private static RedisProcessManager _processManager = new RedisProcessManager();
+        private static ConcurrentDictionary<string, InstanceInfo> _table = new ConcurrentDictionary<string, InstanceInfo>();
+        private static long _nextPort = 2000;
+
         public static InstanceInfo CreateInstance(string instanceId)
         {
+            // Look for existing instance
             InstanceInfo info;
             if (_table.TryGetValue(instanceId, out info))
             {
                 return info;
             }
+
+            // Create a new instance
             info = new InstanceInfo();
             info.instanceId = instanceId;
+
+            // Get unique port
             info.port = (ushort)Interlocked.Increment(ref _nextPort);
-            // TODO: Create process here...
-            //info.processId = whatever;
-            // TODO: Create connectionString
-            //info.connectionString = "redis://username:password@server:port/";
+
+            // Create process
+            var details = new RedisStartDetails
+            {
+                Port = info.port,
+            };
+            info.processId = _processManager.Spawn(details).ProcessId;
+
+            // Create connectionString
+            // TODO: info.connectionString = "redis://username:password@server:port/";
+
+            // Return instance
             _table.TryAdd(instanceId, info); // false means it already existed; should be impossible
             return info;
         }
-
-        private static ConcurrentDictionary<string, InstanceInfo> _table = new ConcurrentDictionary<string, InstanceInfo>();
-        private static long _nextPort = 2000;
-
     }
 
     class InstanceInfo
