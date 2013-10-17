154a3660f27f3f49c5a6ec3d64e7a0fe1d68879b
David Anson <davidans@microsoft.com>
2013-10-17 14:41:17 -0700

    Add simple perf test.

--- WebApi Solution/PerformanceDemo/Program.cs
+++ WebApi Solution/PerformanceDemo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ServiceStack.Redis;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace PerformanceDemo
 {
@@ -6,8 +9,17 @@ namespace PerformanceDemo
     {
         static void Main(string[] args)
         {
-            var client = new RedisClient("angler.redistogo.com", 9313);
-            // ...
+            var client = new RedisClient("crestfish.redistogo.com", 10216, "password");
+            var count = 10;
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (var num in Enumerable.Range(0, count))
+            {
+                var key = num.ToString();
+                client.Add(key, key);
+            }
+            sw.Stop();
+            Console.WriteLine("Time to add {0} key/value pairs: {1}ms", count, sw.ElapsedMilliseconds);
         }
     }
 }
