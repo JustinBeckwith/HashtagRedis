1c33fdf7fc98d1771aa1fff74491fa5e3eb05537
unknown <mkancler@MKANCLER-W8MAC.redmond.corp.microsoft.com>
2013-10-17 16:41:50 -0700

    adding parts of future storage controller

---
+++ WebApi Solution/PerfPage/Storage/StorageController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PerfPage.Storage
+{
+    public class StorageController
+    {
+    }
+}
